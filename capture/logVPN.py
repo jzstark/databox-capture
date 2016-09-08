056e0480704e3b49cad7fb52eb093f63f4f6287e
J.Roger Zhao <rho.ajax@gmail.com>
2016-09-08 14:31:25 -0400

    use http req as VPN capturing example

--- capture/logVPN.py
+++ capture/logVPN.py
@@ -1,42 +1,61 @@
 #!/usr/bin/python
 
+
 from pymongo import MongoClient
+from pktprint import *
+import dpkt
 import pyinotify
 import json
 import pyshark
 import sys
+import datetime
 
 # filename = sys.argv[1]
-header = ["time", "dst_addr", "dstport", "lat", "lon", "src_addr","byte"]
+header = ["time", "src_addr", "src_port", "dst_addr", "dst_port", "length", "http_request"]
 
 client = MongoClient('localhost', 27017)
 db = client.l2tp
-# vpnlog = db.vpnlog
-vpnlog = db.test
+vpnlog = db.vpnlog
 
 class EventHandler(pyinotify.ProcessEvent):
-    #def process_IN_CLOSE_WRITE(self, event):
-    #    print "Modifying: ", event.pathname
-    
-    def process_IN_CREATE(self, event):
-        filename = event.pathname
-        print "Storing:", event.pathname
-        cap = pyshark.FileCapture(filename, display_filter='tcp')
-        for pkt in cap:
-            try:
-                time = pkt.sniff_time
-                dstaddr = pkt.ip.dst
-                dstport = pkt.tcp.dstport
-                lat = pkt.ip.geodst_lat
-                lon = pkt.ip.geodst_lon
-                srcaddr = pkt.ip.src_host
-                byte = pkt.length
 
-                rec = [time, dstaddr, dstport, lat, lon, srcaddr, byte]
-                record = dict(zip(header, rec))
+    def process_IN_CREATE(self, event):
+        # Use capturing HTTP requests as an example
+        filename = event.pathname
+        print "Storing: ", filename
+        pcap = dpkt.pcap.Reader(open(filename))
+        for ts, pkt in pcap: 
+            eth = dpkt.ethernet.Ethernet(pkt)
+            if not isinstance(eth.data, dpkt.ip.IP):
+                print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
+                continue
+            ip = eth.data
+            if isinstance(ip.data, dpkt.tcp.TCP):
+                tcp = ip.data
+                try:
+                    request = dpkt.http.Request(tcp.data)
+                except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
+                    continue
+                do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+                more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+                fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+                
+                srcaddr = inet_to_str(ip.src)
+                srcport = tcp.sport
+                dstaddr = inet_to_str(ip.dst)
+                dstport = tcp.dport 
+                pktlen = len(pkt)
+                httpreq = repr(request)
+
+                data = [ts, srcaddr, srcport, dstaddr, dstport, pktlen, httpreq]
+                record = dict(zip(header, data))
+                # print record
                 result = vpnlog.insert_one(record)
-            except:
-                pass 
+                # print 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(ts))
+                # print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
+                #print 'IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)' % \
+                #                  (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset)
+                #print 'HTTP request: %s\n' % repr(request)
 
     process_IN_CLOSE_WRITE = process_IN_CREATE
 
